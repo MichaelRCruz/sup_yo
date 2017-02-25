75ef203bf2321898b1385b0f5b26c56095ed7014
Michael Cruz <michael.cruz@uwalumni.com>
2017-02-24 21:34:19 -0800

    adds submit form

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
 <template>
   <div>
       <app-header></app-header>
-      <app-footer class="footer"></app-footer>
-      <div class="yo"></div>
+
+        <p class="control">
+          <input class="input is-primary" type="text" placeholder="name" v-model="person.name">
+        </p>
+        <p class="control">
+          <input class="input is-primary" type="text" placeholder="phone" v-model="person.phone">
+        </p>
+        <p class="control">
+          <input class="input is-primary" type="text" placeholder="email" v-model="person.email">
+        </p>
+        <p class="control">
+          <input class="input is-primary" type="text" placeholder="employer" v-model="person.employer">
+        </p>
+        <p class="control">
+          <input class="input is-primary" type="text" placeholder="github" v-model="person.github">
+        </p>
+        <p class="control">
+          <input class="input is-primary" type="text" placeholder="linkedin" v-model="person.linkedin">
+        </p>
+        <p class="control">
+          <input class="input is-primary" type="text" placeholder="facebook" v-model="person.facebook">
+        </p>
+        <a class="button is-primary">Submit Data</a>
+
+        <app-footer class="footer"></app-footer>
+
   </div>
 </template>
 
@@ -12,6 +36,25 @@
     import Card from './Card.vue'
 
     export default {
+        data() {
+          return {
+            person: {
+              name: '',
+              phone: '',
+              email: '',
+              employer: '',
+              github: '',
+              linkedin: '',
+              facebook: '',
+              projects: ''
+            }
+          };
+        },
+        methods: {
+          submit() {
+            
+          }
+        },
         components: {
           'app-header': Header,
           'app-footer': Footer,
@@ -21,9 +64,9 @@
 </script>
 
 <style scoped>
-  .yo {
-    height: 100px;
-    width: 100px;
-    background-color: red;
+
+  .input {
+    width: 30%;
   }
+
 </style>
