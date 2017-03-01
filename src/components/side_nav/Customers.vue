0230a9dbd363cc2b871810e6c0b10e1dd8791368
Michael Cruz <michael.cruz@uwalumni.com>
2017-02-28 18:39:00 -0800

    adds styling to Customers

--- src/components/side_nav/Customers.vue
+++ src/components/side_nav/Customers.vue
@@ -1,59 +1,71 @@
 <template lang="html">
+  <div class="customers">
 
-  <div id="customers" class="animated bounceIn">
-    <div class="tile is-ancestor">
-      <div class="tile is-vertical is-8">
-        <div class="tile">
-          <div class="tile is-parent is-vertical">
-            <article class="tile is-child notification is-primary">
-              <p class="title">Vertical...</p>
-              <p class="subtitle">Top tile</p>
-            </article>
-            <article class="tile is-child notification is-warning">
-              <p class="title">...tiles</p>
-              <p class="subtitle">Bottom tile</p>
-            </article>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child notification is-info">
-              <p class="title">Middle tile</p>
-              <p class="subtitle">With an image</p>
-              <figure class="image is-4by3">
-                <img src="http://bulma.io/images/placeholders/640x480.png">
-              </figure>
-            </article>
-          </div>
-        </div>
-        <div class="tile is-parent">
-          <article class="tile is-child notification is-danger">
-            <p class="title">Wide tile</p>
-            <p class="subtitle">Aligned with the right tile</p>
-            <div class="content">
-              <!-- Content -->
-            </div>
-          </article>
-        </div>
-      </div>
+    <div class="tile is-ancestor c">
       <div class="tile is-parent">
-        <article class="tile is-child notification is-success">
-          <div class="content">
-            <p class="title">Tall tile</p>
-            <p class="subtitle">With even more content</p>
-            <div class="content">
-              <!-- Content -->
-            </div>
-          </div>
-        </article>
+        <div class="tile is-child box is-3">
+          <p class="title">One</p>
+        </div>
+        <div class="tile is-child box is-3">
+          <p class="title">Two</p>
+        </div>
+        <div class="tile is-child box is-3">
+          <p class="title">Two</p>
+        </div>
+        <div class="tile is-child box is-3">
+          <p class="title">Two</p>
+        </div>
+        <div class="tile is-child box is-3">
+          <p class="title">Two</p>
+        </div>
+        <div class="tile is-child box is-3">
+          <p class="title">Two</p>
+        </div>
       </div>
-      </div>
-  </div>
+    </div>
 
+  </div>
 </template>
 
 
 <script>
-export default {
-}
+  export default {
+    data() {
+      return {
+        user: {
+          name: '',
+          github_avatar_url: '',
+          phone: '',
+          email: '',
+          employer: '',
+          github: '',
+          linkedin: '',
+          facebook: '',
+          projects: ''
+        },
+        users: []
+      };
+    },
+    methods: {
+      submit() {
+        this.$http.post('', this.user)
+          .then(response => {
+            console.log(response);
+          }, error => {
+            console.log(error);
+          });
+      },
+      fetchData() {
+        this.$http.get('')
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+            this.users = data;
+          });
+      }
+    }
+  }
 </script>
 
 
--- src/components/side_nav/Invitations.vue
+++ src/components/side_nav/Invitations.vue
@@ -16,15 +16,6 @@
               </figure>
             </article>
           </div>
-          <!-- <div class="tile is-parent">
-            <article class="tile is-child notification is-info">
-              <p class="title">Middle tile</p>
-              <p class="subtitle">With an image</p>
-              <figure class="image is-4by3">
-                <img src="http://bulma.io/images/placeholders/640x480.png">
-              </figure>
-            </article>
-          </div> -->
 
         </div>
       </div>
