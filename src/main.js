1562c2ad81e87604a8ed6f71d1cb95d27993e737
Michael Cruz <michael.cruz@uwalumni.com>
2017-03-07 00:26:08 -0800

    intercepts http request for cookies

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ Vue.use(VueRouter);
 Vue.use(VueResource);
 
 Vue.http.options.root = 'http://localhost:3000/api'
+Vue.http.interceptors.push((request, next) => {
+    console.log(request)
+    request.credentials = true;
+    next();
+});
 
 const router = new VueRouter({
   routes,
