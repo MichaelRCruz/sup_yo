7a79c0ee3338c09754f9888a26618c155020f299
Michael Cruz <michael.cruz@uwalumni.com>
2017-03-08 14:00:23 -0800

    cleans up code

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
-import App from './App.vue'
-import { routes } from './routes'
-import { store } from './store/store'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import App from './App.vue';
+import { routes } from './routes';
+import { store } from './store/store';
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
