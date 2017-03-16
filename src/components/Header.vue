413f5e1582918aa027f0166c25be7543b20f3c00
Michael Cruz <michael.cruz@uwalumni.com>
2017-03-15 23:13:58 -0700

    hides home tab

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,8 +7,8 @@
           <a class="nav-item">
             <img src="" alt="sup_yo logo">
           </a>
-          <router-link to="/home" class="nav-item is-tab is-hidden-mobile" active-class="is-active" exact>Home</router-link>
-          <router-link to="/dashboard" class="nav-item is-tab is-hidden-mobile" active-class="is-active" exact>Dashboard</router-link>
+          <!-- <router-link to="/home" class="nav-item is-tab is-hidden-mobile" active-class="is-active" exact>Home</router-link> -->
+          <router-link to="/dashboard" class="nav-item is-tab is-hidden-mobile is-active" exact>Dashboard</router-link>
           <router-link to="/dashboard" class="nav-item is-tab is-hidden-mobile"></router-link>
         </div>
         <span class="nav-toggle">
