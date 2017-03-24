efc9bb4e389d5f852ab5514ce49fe401b0913512
Michael Cruz <michael.cruz@uwalumni.com>
2017-03-23 23:32:09 -0700

    Serve index.html by default

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+
 app = express();
-app.use(serveStatic(__dirname));
+app.use(serveStatic(__dirname, {
+  fallthrough: true
+}));
+app.use((req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
